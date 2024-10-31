892711652b53e7e0fa2098af1bfc94778a7a758f
menukipeiris <menukimpeiris@gmail.com>
2024-10-31 09:51:29 +0530

    Project created

---
+++ src/test/java/lk/ijse/aad/greenShadow/GreenShadowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad.greenShadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
