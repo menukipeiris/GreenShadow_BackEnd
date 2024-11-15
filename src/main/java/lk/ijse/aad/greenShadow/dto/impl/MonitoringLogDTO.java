84a2b1bf15733e11e6ddc6d085c5da8f4df48e91
menukipeiris <menukimpeiris@gmail.com>
2024-11-15 22:11:35 +0530

    DTO classes added

---
+++ src/main/java/lk/ijse/aad/greenShadow/dto/impl/MonitoringLogDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.aad.greenShadow.dto.impl;
+
+import lk.ijse.aad.greenShadow.dto.MonitoringLogStatus;
+import lk.ijse.aad.greenShadow.entity.impl.CropEntity;
+import lk.ijse.aad.greenShadow.entity.impl.FieldEntity;
+import lk.ijse.aad.greenShadow.entity.impl.StaffEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MonitoringLogDTO implements MonitoringLogStatus {
+    private String logCode;
+    private String logDate;
+    private String logDetails;
+    private String observedImage;
+    private List<FieldEntity> fields;
+    private List<CropEntity> crops;
+    private List<StaffEntity> staff;
+}
