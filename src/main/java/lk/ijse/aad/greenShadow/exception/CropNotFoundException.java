d5c238d99f3d225845636493ade0c65a0fcb5c1e
menukipeiris <menukimpeiris@gmail.com>
2024-11-17 16:23:02 +0530

    Added exception

---
+++ src/main/java/lk/ijse/aad/greenShadow/exception/CropNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad.greenShadow.exception;
+
+public class CropNotFoundException extends RuntimeException{
+    public CropNotFoundException() {
+    }
+
+    public CropNotFoundException(String message) {
+
+    }
+    public CropNotFoundException(String message, Throwable cause) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/aad/greenShadow/exception/FieldNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad.greenShadow.exception;
+
+public class FieldNotFoundException extends RuntimeException{
+    public FieldNotFoundException() {
+
+    }
+
+    public FieldNotFoundException(String message) {
+    }
+
+    public FieldNotFoundException(String message, Throwable cause) {
+    }
+}
