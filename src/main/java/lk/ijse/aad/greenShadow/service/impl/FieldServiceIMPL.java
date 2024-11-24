18fbe03bf8494b70003800092522b2d09b74015f
menukipeiris <menukimpeiris@gmail.com>
2024-11-24 10:40:54 +0530

    Implemented update allocated staff

--- src/main/java/lk/ijse/aad/greenShadow/service/impl/FieldServiceIMPL.java
+++ src/main/java/lk/ijse/aad/greenShadow/service/impl/FieldServiceIMPL.java
@@ -1,12 +1,18 @@
 package lk.ijse.aad.greenShadow.service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.aad.greenShadow.customStatusCode.SelectedErrorStatus;
 import lk.ijse.aad.greenShadow.dao.FieldDAO;
+import lk.ijse.aad.greenShadow.dao.StaffDAO;
+import lk.ijse.aad.greenShadow.dto.FieldStatus;
 import lk.ijse.aad.greenShadow.dto.impl.FieldDTO;
+import lk.ijse.aad.greenShadow.entity.impl.CropEntity;
 import lk.ijse.aad.greenShadow.entity.impl.FieldEntity;
-import lk.ijse.aad.greenShadow.exception.DataPersistException;
+import lk.ijse.aad.greenShadow.entity.impl.StaffEntity;
 import lk.ijse.aad.greenShadow.exception.FieldNotFoundException;
+import lk.ijse.aad.greenShadow.exception.StaffNotFoundException;
 import lk.ijse.aad.greenShadow.service.FieldService;
+import lk.ijse.aad.greenShadow.util.AppUtil;
 import lk.ijse.aad.greenShadow.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,27 +27,79 @@ public class FieldServiceIMPL implements FieldService {
     private FieldDAO fieldDao;
     @Autowired
     private Mapping fieldMapping;
+    @Autowired
+    private StaffDAO staffDao;
+
     @Override
-    public void saveField(FieldDTO buildFieldDTO) {
-        FieldEntity savedField =fieldDao.save(fieldMapping.toFieldEntity(buildFieldDTO));
-        if (savedField == null) {
-            throw new DataPersistException("Field is not saved!");
+    public void saveField(FieldDTO fieldDTO) {
+        fieldDTO.setFieldCode(AppUtil.generateFieldId());
+        FieldEntity fieldEntity = fieldDao.save(fieldMapping.toFieldEntity(fieldDTO));
+        if (fieldEntity == null) {
+            throw new FieldNotFoundException("Field not found");
         }
     }
 
     @Override
-    public void uploadImages(String fieldCode, FieldDTO buildFieldDTO) {
-        Optional<FieldEntity> fieldField =fieldDao.findById(fieldCode);
-        if (fieldField.isPresent()) {
-            fieldField.get().setFieldImage1(buildFieldDTO.getFieldImage1());
-            fieldField.get().setFieldImage2(buildFieldDTO.getFieldImage2());
+    public List<FieldDTO> getAllFields() {
+        return fieldMapping.toFieldDTOList(fieldDao.findAll());
+    }
+
+    @Override
+    public FieldStatus getField(String fieldCode) {
+        if(fieldDao.existsById(fieldCode)){
+            var selectedField = fieldDao.getReferenceById(fieldCode);
+            return fieldMapping.toFieldDTO(selectedField);
         }else {
-            throw new FieldNotFoundException("This Field-" + fieldCode + " is not found");
+            return new SelectedErrorStatus(2,"Selected field not found");
         }
     }
 
     @Override
-    public List<FieldDTO> getAllField() {
-        return fieldMapping.asFieldDTOList(fieldDao.findAll());
+    public void deleteField(String fieldCode) {
+        Optional<FieldEntity> foundField = fieldDao.findById(fieldCode);
+        if(foundField.isPresent()){
+            throw new FieldNotFoundException("Field not found");
+        }else {
+            fieldDao.deleteById(fieldCode);
+        }
+    }
+
+    @Override
+    public void updateField(String fieldCode, FieldDTO fieldDTO) {
+        Optional<FieldEntity> tmpField = fieldDao.findById(fieldCode);
+        if(!tmpField.isPresent()){
+            throw new FieldNotFoundException("Field not found");
+        }else{
+            tmpField.get().setFieldName(fieldDTO.getFieldName());
+            tmpField.get().setLocation(fieldDTO.getLocation());
+            tmpField.get().setExtentSize(fieldDTO.getExtentSize());
+            tmpField.get().setFieldImage1(fieldDTO.getFieldImage1());
+            tmpField.get().setFieldImage2(fieldDTO.getFieldImage2());
+            List<CropEntity> cropEntityList = fieldMapping.toCropEntityList(fieldDTO.getCrops());
+            tmpField.get().setCrops(cropEntityList);
+            List<StaffEntity> staffEntityList = fieldMapping.toStaffEntityList(fieldDTO.getAllocated_staff());
+            tmpField.get().setStaff(staffEntityList);
+        }
+    }
+
+    @Override
+    public void updateAllocatedStaff(String fieldCode,List<String> staffId) {
+        Optional<FieldEntity> tmpField = fieldDao.findById(fieldCode);
+        if(!tmpField.isPresent()){
+            throw new FieldNotFoundException("Field not found");
+        }
+        FieldEntity fieldEntity = tmpField.get();
+        for(String staff : staffId){
+            Optional<StaffEntity> tmpStaff = staffDao.findById(staff);
+            if(!tmpStaff.isPresent()){
+                throw new StaffNotFoundException("Staff not found");
+            }else{
+                StaffEntity staffEntity = tmpStaff.get();
+                fieldEntity.getStaff().add(staffEntity);
+                staffEntity.getFields().add(fieldEntity);
+                staffDao.save(staffEntity);
+            }
+        }
+        fieldDao.save(fieldEntity);
     }
 }
