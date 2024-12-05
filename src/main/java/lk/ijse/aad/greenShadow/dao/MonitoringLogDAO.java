443f2fcdecc434dbc2bd34604eddf23471a56d90
menukipeiris <menukimpeiris@gmail.com>
2024-12-05 12:28:41 +0530

    Added findByLogDescMethod

--- src/main/java/lk/ijse/aad/greenShadow/dao/MonitoringLogDAO.java
+++ src/main/java/lk/ijse/aad/greenShadow/dao/MonitoringLogDAO.java
@@ -2,8 +2,14 @@ package lk.ijse.aad.greenShadow.dao;
 
 import lk.ijse.aad.greenShadow.entity.impl.MonitoringLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MonitoringLogDAO extends JpaRepository<MonitoringLogEntity,String> {
+    @Query("SELECT l FROM MonitoringLogEntity l WHERE l.logDetails = :logDesc")
+    Optional<MonitoringLogEntity> findByLogDesc(@Param("logDesc") String logDesc);
 }
